b7aff430879dfe9cca541e9c68813bfa2a173cf4
User han <handevmin@gmail.com>
2024-11-21 11:46:43 +0900

    API 주소 변경

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://culf-test-bot-53526c2de9f6.herokuapp.com';
 
 export async function uploadImage(file) {
   const formData = new FormData();
